660a5b8699b864ee00aa25540df952fc32629e7a
Abraham Raphael <abrahamraphael1995@gmail.com>
2025-09-18 20:38:44 +0100

    usercontext corrected

--- alx-react-app-props/src/context/UserContext.js
+++ alx-react-app-props/src/context/UserContext.js
@@ -1,6 +1,6 @@
-// src/UserContext.js
-import React from 'react';
+// src/context/UserContext.js
+import { createContext } from 'react';
 
-const UserContext = React.createContext();
+const UserContext = createContext();
 
 export default UserContext;
