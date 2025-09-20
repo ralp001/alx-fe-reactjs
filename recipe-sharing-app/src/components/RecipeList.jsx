8f08d5192fc6f314aa9322ac567d8a5c11e30b5c
Abraham Raphael <abrahamraphael1995@gmail.com>
2025-09-20 10:52:40 +0100

    feat: Implement basic recipe app with Zustand

---
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -0,0 +1,20 @@
+// src/components/RecipeList.jsx
+import useRecipeStore from '../store/useRecipeStore';
+
+const RecipeList = () => {
+  const recipes = useRecipeStore(state => state.recipes);
+
+  return (
+    <div>
+      <h2>Recipe List</h2>
+      {recipes.map(recipe => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RecipeList;
---
+++ recipe-sharing-app/src/store/useRecipeStore.js
@@ -0,0 +1,10 @@
+// src/store/useRecipeStore.js
+import { create } from 'zustand';
+
+const useRecipeStore = create(set => ({
+  recipes: [],
+  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
+  setRecipes: (recipes) => set({ recipes })
+}));
+
+export default useRecipeStore;
